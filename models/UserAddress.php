b9b9ec072283a6e50eb6e3dc747661615802bfac
dinhthinh6 <dthinh3005@gmail.com>
2024-07-09 11:56:46 +0700

    refactor: refactoring base model, model, query model

---
+++ models/UserAddress.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\models;
+
+use \app\models\base\UserAddress as BaseUserAddress;
+
+/**
+ * This is the model class for table "user_address".
+ */
+class UserAddress extends BaseUserAddress
+{
+
+}
---
+++ models/query/UserAddressQuery.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\models\query;
+
+use \app\models\UserAddress;
+
+/**
+ * This is the ActiveQuery class for [[UserAddress]].
+ *
+ * @see \app\models\UserAddress
+ * @method UserAddress[] all($db = null)
+ * @method UserAddress one($db = null)
+ */
+class UserAddressQuery extends \yii\db\ActiveQuery
+{
+
+}
